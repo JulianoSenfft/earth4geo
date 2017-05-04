1061abcf880ff66f152eb8b13793fc1368a1cfec
Juliano Senfft <juliano.senfft@hotmail.com>
2017-05-04 10:04:11 -0300

    Fix

--- apps/web/header.php
+++ apps/web/header.php
@@ -39,9 +39,9 @@
                     </a>
                 </div>
                 <ul class="nav navbar-nav">
-                    <li><a href="index-user.php">Seja Premium</a></li>
                 </ul>
                 <ul class="nav navbar-nav right-navbar-nav">
+                    <li><a href="index-user.php">Seja Premium</a></li>
                     <li><a href="minha-conta.php">Minha Conta</a></li>
                     <li><a href="sair.php">Sair</a></li>
                 </ul>
