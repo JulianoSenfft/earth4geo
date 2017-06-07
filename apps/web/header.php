2f172dd94444b3c10c6b68e4f9a9fad7249f6f7f
Juliano Senfft <juliano.senfft@hotmail.com>
2017-06-07 13:44:35 -0300

    Register page

--- apps/web/header.php
+++ apps/web/header.php
@@ -66,6 +66,7 @@
                 <ul class="nav navbar-nav right-navbar-nav">
                     <li><a href="index-user.php">Seja Premium</a></li>
                     <li><a href="login.php">Login</a></li>
+                    <li><a href="registrar.php">Registrar</a></li>
                     <li><a href="minha-conta.php">Minha Conta</a></li>
                     <li><a href="sair.php">Sair</a></li>
                 </ul>
